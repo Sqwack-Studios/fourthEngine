b9cfa05809b9767b6f820fd84bfa344f211c20f8
wildpaco <transistordaniel10@gmail.com>
2023-10-05 18:00:49 +0200

    cleaned FFT hlsl includes to start over

--- fourth_Engine/Shaders/Compute/FFT_CS.hlsl
+++ fourth_Engine/Shaders/Compute/FFT_CS.hlsl
@@ -1,4 +1,3 @@
-
 #include "FFTs.hlsli"
 
 RWTexture2D<float4> input : register(UAV_PP_COMPUTE_0);
@@ -23,26 +22,5 @@ cbuffer fftImageDsc : register(CB_PP_0)
 [numthreads(WORKGROUP_SIZE_X, 1, 1)]
 void main(uint3 id : SV_DispatchThreadID, uint3 tid : SV_GroupThreadID, uint3 gid : SV_GroupID)
 {
-	uint threadIndex = tid.x;
-	uint groupIndex = gid.x;
-	uint2 uv = uint2(threadIndex, groupIndex);
-	float3 src = input[uv].rgb;
 
-	float2 input[3] = { float2(src.x, 0.0f), float2(src.y, 0.0f), float2(src.z, 1.0f) };
-	float2 output[3] = { float2(0.0f, 0.0f), float2(0.0f, 0.0f), float2(0.0f, 0.0f) };
-
-	bool isForward = true;
-
-	for (uint channel = 0; channel < 3; ++channel)
-	{
-		float2 f_n = input[channel];
-		float2 F_k = CooleyTukeyFFT(f_n, threadIndex, g_log2_fft_size, g_fft_size.x, g_isForward);
-		output[channel] = F_k;
-	}
-
-	float4 outputReal_imX = float4(output[0], output[1]);
-	float2 imYZ = output[2];
-
-	realRGBimagA[uv] = outputReal_imX;
-	imagRG[uv] = imYZ;
 }
