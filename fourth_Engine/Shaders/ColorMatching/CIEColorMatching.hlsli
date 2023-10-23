ca93ca49e672c050e1ea39f8703fe2d117978064
wildpaco <transistordaniel10@gmail.com>
2023-10-23 18:28:21 +0200

    first draft of the dispersion shader finished

--- fourth_Engine/Shaders/ColorMatching/CIEColorMatching.hlsli
+++ fourth_Engine/Shaders/ColorMatching/CIEColorMatching.hlsli
@@ -1,3 +1,5 @@
+#include "DxyIlluminant_helpers.hlsli"
+
 #ifndef _CIE_COLOR_MATCHING_HLSLI
 #define _CIE_COLOR_MATCHING_HLSLI
 
@@ -57,135 +59,17 @@ float3 RGB_to_XYZ(in const float3 RGB)
      return mul(RGB, CMF);
 }
 
-//The D illuminant can be used to approximate the daylight spectrum between 4000K and 25,000K. 
-//The inputs needed are the xy(not XYZ) coordinates from the 1931 CIE coordinate system, along with the three functions named S0 (blue), S1 (green) and S2 (red)
-
-void compute_D_Illuminant_M1M2(in const float2 CIEcoords, inout float M1, inout float M2)
+float sample_Dxy_illuminant(in const float wavelength, in const float2 xyCoords)
 {
-     float invDenominator = 1.0f / (0.0241f + 0.2562 * CIEcoords.x - 0.7341f * CIEcoords.y);
-     M1 = (-1.3515f  -  1.7703f * CIEcoords.x +  5.9114f * CIEcoords.y) * invDenominator;
-     M2 = ( 0.0300f - 31.4424f * CIEcoords.x + 30.0717f * CIEcoords.y) * invDenominator;
+    const uint idx = getIlluminantD_wavelengthIndex(wavelength);
+    
+    float M1, M2;
+    compute_D_Illuminant_M1M2(xyCoords, M1, M2);
+    
+    float3 S012 = D_S012[idx];
+    
+    return spectrum_Dxy_Illuminant(M1, M2, S012.x, S012.y, S012.z);
 }
-
-float spectrum_Dxy_Illuminant(in const float M1, in const float M2, in const float S0, in const float S1, in const float S2)
-{
-    return S0 + S1 * M1 + S2 * M2;
-}
-
-float computeColorCorrelatedTemperatureFromCromaticity(in const float2 xy)
-{
-     const float xe = 0.3320f;
-     const float ye = 0.1858f;
-     const float n = (xy.x - xe)/(xy.y - ye);
-
-     const float n2 = n * n;
-     const float n3 = n2 * n;
-
-     return -449.0f * n3 + 3525.0f * n2 - 6823.3f * n + 5520.33f;
-}
-
-//TODO: float2 computeCromaticityFromColorCorrelatedTemperature(in const float CCT)
-
-//---------D illuminant S0, S1, S2 curves. Each index represents a wavelength, each step is 5nm. If a value gets past or under the [360, 780] range, it gets clamped.
-//wavelength start: 380nm
-//wavelength end:   780nm
-
-//TOOD: we could make a triple sample by interpolating bewteen wave_n-1, wave_n, wave_n+1
-uint getIlluminantD_wavelengthIndex(in const float wavelength)
-{
-    const float maxWave = 780;
-    const float minWave = 380;
-    const uint waveStep = 5;
-
-    uint index = clamp((wavelength - minWave) / waveStep, minWave, maxWave);
-
-    return index;
-}
-
-static const float3 D_S012[80] = 
-{ float3( 63.40f,  38.50f, 3.0f),
-  float3( 64.60f,  36.75f,	 2.10f),
-  float3( 65.80f,  35.00f,	 1.20f),
-  float3( 80.30f,  39.20f,	 0.05f),
-  float3( 94.80f,  43.40f,	-1.10f),
-  float3( 99.80f,  44.85f,	-0.80f),
-  float3(104.80f,   6.30f,	-0.50f),
-  float3(105.3f ,  45.10f,  -0.60f),
-  float3(105.9f ,  43.90f,  -0.70f),
-  float3(101.3f ,  40.50f,  -0.95f),
-  float3(96.80f ,  37.10f,  -1.20f),
-  float3(105.3f ,  36.90f,  -1.90f),
-  float3(113.9f ,  36.70f,  -2.60f),
-  float3(119.7f ,  36.30f,  -2.75f),
-  float3(125.6f ,  35.90f,  -2.90f),
-  float3(125.5f ,  34.25f,  -2.85f),
-  float3(125.5f ,  32.60f,  -2.80f),
-  float3(123.4f ,  30.25f,  -2.70f),
-  float3(121.3f ,  27.90f,  -2.60f),
-  float3(121.3f ,  26.10f,  -2.60f),
-  float3(121.3f ,  24.30f,  -2.60f),
-  float3(117.4f ,  22.20f,  -2.20f),
-  float3(113.5f ,  20.10f,  -1.80f),
-  float3(113.3f ,  18.15f,  -1.65f),
-  float3(113.1f ,  16.20f,  -1.50f),
-  float3(111.9f ,  14.70f,  -1.40f),
-  float3(110.8f ,  13.20f,  -1.30f),
-  float3(108.6f ,  10.90f,  -1.25f),
-  float3(106.5f ,  8.60f,   -1.20f),
-  float3(107.6f ,  7.35f,   -1.10f),
-  float3(108.8f ,  6.10f,   -1.00f),
-  float3(107.0f ,  5.15f,   -0.75f),
-  float3(105.3f ,  4.20f,   -0.50f),
-  float3(104.8f ,  3.05f,   -0.40f),
-  float3(104.4f ,  1.90f,   -0.30f),
-  float3(102.2f ,  0.95f,   -0.15f),
-  float3(100.0f ,  0.00f,    0.00f),
-  float3( 98.00f,  -0.80f,   0.10f  ),
-  float3( 96.00f,  -1.60f,   0.20f  ),
-  float3( 95.55f,  -2.55f,   0.35f  ),
-  float3( 95.10f,  -3.50f,   0.50f  ),
-  float3( 92.10f,  -3.50f,   1.30f  ),
-  float3( 89.10f,  -3.50f,   2.10f  ),
-  float3( 89.80f,  -4.65f,   2.65f  ),
-  float3( 90.50f,  -5.80f,   3.20f  ),
-  float3( 90.40f,  -6.50f,   3.65f  ),
-  float3( 90.30f,  -7.20f,   4.10f  ),
-  float3( 89.35f,  -7.90f,   4.40f  ),
-  float3( 88.40f,  -8.60f,   4.70f  ),
-  float3( 86.20f,  -9.05f,   4.90f  ),
-  float3( 84.00f,  -9.50f,   5.10f  ),
-  float3( 84.55f, -10.20f,   5.90f  ),
-  float3( 85.10f, -10.90f,   6.70f  ),
-  float3( 83.50f, -10.80f,   7.00f  ),
-  float3( 81.90f, -10.70f,   7.30f  ),
-  float3( 82.25f, -11.35f,   7.95f  ),
-  float3( 82.60f, -12.00f,   8.60f  ),
-  float3( 83.75f, -13.00f,   9.20f  ),
-  float3( 84.90f, -14.00f,   9.80f  ),
-  float3( 83.10f, -13.80f,   10.00f ),
-  float3( 81.30f, -13.60f,   10.20f ),
-  float3( 76.60f, -12.80f,   9.25f  ),
-  float3( 71.90f, -12.00f,   8.30f  ),
-  float3( 73.10f, -12.65f,   8.95f  ),
-  float3( 74.30f, -13.30f,   9.60f  ),
-  float3( 75.35f, -13.10f,   9.05f  ),
-  float3( 76.40f, -12.90f,   8.50f  ),
-  float3( 69.85f, -11.75f,   7.75f  ),
-  float3( 63.30f, -10.60f,   7.00f  ),
-  float3( 67.50f, -11.10f,   7.30f  ),
-  float3( 71.70f, -11.60f,   7.60f  ),
-  float3( 74.35f, -11.90f,   7.80f  ),
-  float3( 77.00f, -12.20f,   8.00f  ),
-  float3( 71.10f, -11.20f,   7.35f  ),
-  float3( 65.20f, -10.20f,   6.70f  ),
-  float3( 56.45f,  -9.00f,   5.95f  ),
-  float3( 47.70f,  -7.80f,   5.20f  ),
-  float3( 58.15f,  -9.50f,   6.30f  ),
-  float3( 68.60f, -11.20f,   7.40f  ),
-  float3( 66.80f, -10.80f,   7.10f  ),
-  float3( 65.00f, -10.40f,   6.80f  )
-};
-
 
 
 #endif //_CIE_COLOR_MATCHING_HLSLI
--- fourth_Engine/Shaders/KernelGeneration/ApertureDispersion_CS.hlsl
+++ fourth_Engine/Shaders/KernelGeneration/ApertureDispersion_CS.hlsl
@@ -1,10 +1,15 @@
 #include "../Common.hlsli"
-
+#include "../ColorMatching/CIEColorMatching.hlsli"
 #ifndef _APERTURE_DISPERSION_CS_HLSL
 #define _APERTURE_DISPERSION_CS_HLSL
 
+#define TILE_SIZE 8
+
 Texture2D<float>           sourcePattern : register(SRV_PP_0);
 RWTexture2D<float3> polychromaticPattern : register(UAV_PP_COMPUTE_0);
+
+static const float sampleWave[3] = { 0.680f, 0.530f, 0.460f }; //RGB wavelengths
+
 
 //This shader performs polychromatic dispersion to the Fourier Transform of an aperture under the assumptions of Far Field Fraunhofer diffraction
 // source : https://onlinelibrary.wiley.com/doi/10.1111/j.1467-8659.2009.01357.x
@@ -14,50 +19,77 @@ RWTexture2D<float3> polychromaticPattern : register(UAV_PP_COMPUTE_0);
 //
 // For each wavelength, we will also assume that the aperture was lit by a standard Illuminant D65, so we will also sample a relative SPD_65, transform 
 // into XYZ coordinates and then into RGB. Finally, a energy conservation normalization could be employed.
-//-----------------------------------------------------------Inputs---------------------------------------------------------------
-//
-//
-//      ------>Texture2D<float> sourcePattern: it's the output of the Fourier Transform. The scale is important, as we have to make a lot of operations over float types.
-//            Applying arithmetic operations to very different floats in size can rapidly cause precision issues
-//
-//      ------>RWTexture2D<float3> polychromaticPattern: polychromatic pattern affected by dispersion, assuming Fraunhofer diffraction.
-//
-//     -----------------------------------------Uniforms------------------------------
-//
-//     -------> uint     g_numSamples      : num of visible spectrum wavelengths to sample. This is the number of texture lookups per destination texel.
-//     -------> float2    g_signalLength   : signalLength.x, invSingalLength.y
-//     -------> float1   g_maxFrequency    : maximum spatial frequency representable by the physical shape of the signal receiver (sensor/human eye). It also depends of the resolution of the FFT
-//                                         maxFreq = length/physicalSize * 0.5
-//     -------> float1   g_frequencyStep   : size of the FFT bins. Each texel evaluated at the center is a step of 1/physicalSize
-//     -------> float1   g_focalDistance   : distance between the sensore receiver and the plane/mask/aperture responsible for the diffraction. It is needed to evaluate the spatial domain of 
-//                                         the diffraction pattern. The relationship is based on fraunhofer diffraction: freq = x / (wavelength * focalDistance)
-//     
-//
-//  //TODO: Add fresnel term. Deduce how to maybe speedup things using LDS, because we will be probably sampling the same texel multiple times.
-//
+// |----------------------------------------------------------Inputs---------------------------------------------------------------
+// |NOTE: spatial variables are expected in micrometers.
+// |
+// |     ------>Texture2D<float> sourcePattern: it's the output of the Fourier Transform. The scale is important, as we have to make a lot of operations over float types.
+// |           Applying arithmetic operations to very different floats in size can rapidly cause precision issues
+// |
+// |     ------>RWTexture2D<float3> polychromaticPattern: polychromatic pattern affected by dispersion, assuming Fraunhofer diffraction.
+// |
+// |    ************************************************  Uniforms  ************************************************
+// |
+// |    -------> uint     g_numSamples      : num of visible spectrum wavelengths to sample. This is the number of texture lookups per destination texel.
+// |    -------> float2   g_signalLength    : signalLength.x, invSingalLength.y
+// |    -------> float1   g_maxFrequency    : maximum spatial frequency representable by the physical shape of the signal receiver (sensor/human eye). It also depends of the resolution of the FFT
+// |                                        maxFreq = length/physicalSize * 0.5
+// |    -------> float1   g_frequencyStep   : size of the FFT bins. Each texel evaluated at the center is a step of 1/physicalSize
+// |    -------> float1   g_focalDistance   : distance between the sensore receiver and the plane/mask/aperture responsible for the diffraction. It is needed to evaluate the spatial domain of 
+// |                                        the diffraction pattern. The relationship is based on fraunhofer diffraction: freq = x / (wavelength * focalDistance)
+// |    -------> float1   g_spatialStep     : physical size of each sensor pixel
+// |    -------> float2   g_kernelCenter    : UV coordinates that define where is the center of the kernel. This is used to match frequency/space coordinates and find the center
+// | //TODO: Add fresnel term. Deduce how to maybe speedup things using LDS, because we will be probably sampling the same texel multiple times.
+// |
+// |
+// |
+// |------------------------------------------------------------------------------------------------------------------------------------
+
+
 cbuffer dispersionParams : register(CB_PP_0)
 {
     uint     g_numSamples;
     float2   g_signalLength;
     float    g_maxFrequency;
+    //
     float    g_frequencyStep;
     float    g_focalDistance;
+    float    g_spatialStep;
+    float    pad0;
+    //
+    float2   g_kernelCenter;
 };
 
-#define TILE_SIZE 8
+
 [numthreads(TILE_SIZE, TILE_SIZE, 1)]
 void main(uint3 DTid : SV_DispatchThreadID)
 {
-    const uint2 texel = DTid.xy; //This is the texel were we want to compute the dispersion
+    const uint2 targetTexel = DTid.xy; //This is the texel were we want to compute the dispersion
+
+    if (targetTexel.x >= uint(g_signalLength.x) || targetTexel.y >= uint(g_signalLength.y))
+        return;
 
     const uint signalLength = float(g_signalLength.x);
     const float invSignalLength = g_signalLength.y;
     
-    for (uint s = 0; s < g_numSamples; ++s)
+    //We have to compute the position of this texel in the sensor. Then, for each wavelength, we compute which frequency is that, and sample.
+    const float2 position = (float2(targetTexel) - g_kernelCenter * float(signalLength.x)) * g_spatialStep;
+    
+    float3 finalColor = float3(0.0f, 0.0f, 0.0f);
+    
+    for (uint s = 0; s < 3; ++s)
     {
+        const float wavelength_um = sampleWave[s];
+        const float wavelength_nm = wavelength_um * 1e+3f;
+        const float2 frequencyUV = position / (g_focalDistance * wavelength_um) + g_kernelCenter; //fourier domain coords displaced by the kernel center
         
+        //now we sample D65 illuminant for this wavelength
+        //const float D65_spectrum = sample_Dxy_illuminant(wavelength_nm, CIE1931_D65_xy);
+        const float3 XYZ = float3(X_Fit_1931(wavelength_nm), Y_Fit_1931(wavelength_nm), Z_Fit_1931(wavelength_nm));
+
+        finalColor += XYZ_to_RGB(XYZ) * sourcePattern.SampleLevel(g_bilinearClamp, frequencyUV, 0.0f);
     }
 
+    polychromaticPattern[targetTexel] = finalColor;
 }
 
 
